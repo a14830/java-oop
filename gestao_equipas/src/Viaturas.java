e3fa59cb3da0c467b8707df481ae9bffe1c84d37
a14830 <a14830@oficina.pt>
2026-04-27 09:50:55 +0000

    fghfgh

---
+++ gestao_equipas/src/Viaturas.java
@@ -0,0 +1,20 @@
+class Viatutas {
+    String tipo;
+    String rodas;
+    int velMax;
+
+    public Viatutas() {
+    }
+
+    public Viatutas(String tipo, String rodas, int velMax) {
+        this.tipo = tipo;
+        this.rodas = rodas;
+        this.velMax = velMax;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+    
+}
+

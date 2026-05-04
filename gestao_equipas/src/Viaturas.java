e452cfb9c8f9cb1a34e1cab96ff17c9f99ca97d8
a14830 <a14830@oficina.pt>
2026-05-04 09:19:04 +0100

    Fix class name from Viatutas to Viaturas

--- gestao_equipas/src/Viaturas.java
+++ gestao_equipas/src/Viaturas.java
@@ -1,12 +1,12 @@
-class Viatutas {
+class Viaturas {
     String tipo;
     String rodas;
     int velMax;
 
-    public Viatutas() {
+    public Viaturas() {
     }
 
-    public Viatutas(String tipo, String rodas, int velMax) {
+    public Viaturas(String tipo, String rodas, int velMax) {
         this.tipo = tipo;
         this.rodas = rodas;
         this.velMax = velMax;
@@ -15,6 +15,30 @@ class Viatutas {
     public String getTipo() {
         return tipo;
     }
-    
+
+    public String getRodas() {
+        return rodas;
+    }
+
+    public int getVelMax() {
+        return velMax;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setRodas(String rodas) {
+        this.rodas = rodas;
+    }
+
+    public void setVelMax(int velMax) {
+        this.velMax = velMax;
+    }
+
+    @Override
+    public String toString() {
+        return "Tipo: " + tipo + ", Rodas: " + rodas + ", VelMax: " + velMax;
+    }
 }
 

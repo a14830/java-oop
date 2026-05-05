677a6809c1000bd924ef43129efc3c231e9f0dfd
a14830 <a14830@oficina.pt>
2026-05-05 08:57:02 +0000

    polifismos e heranças

--- gestao_equipas/src/Barco.java
+++ gestao_equipas/src/Barco.java
@@ -1,26 +1,22 @@
 public class Barco extends Viaturas {
 
-private boolean temPiscina;
+    private boolean temPiscina;
 
-    public Barco (){
-        super("barco", 0, 0, "");
-}
+    public boolean isTemPiscina() {
+        return temPiscina;
+    }
+
+    public void setTemPiscina(boolean temPiscina) {
+        this.temPiscina = temPiscina;
+    }
+
+    public Barco () {
+        super("Barco", 0, 0);
+    }
     
-    public boolean isTemPiscina(){
-    return temPiscina;
-}
-    public void setTemPiscina(boolean temPiscina){
-    this.temPiscina = temPiscina;
-}
+    @Override
+    void mostraInfo (){
+        System.out.println("Info do carro: \n Tipo viatura:" + getViatura() + "\n numRodas: " + getNumRodas() + "\n velMax. " + getVelMax());
+    }
 
-@Override
-    public String toString() {
-    return ("Viaturas tipo da viatura: " + getViatura() +  ", numero de rodas: " + getNumRodas() + ", velocidade maxima: " + getVelMax() + "");
-}
-    if (temPiscina){
-        System.out.println("tem Piscina");
-    }
-    else {
-        System.out.println("nao tem piscina");
-    }
 }
